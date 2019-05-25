a37ce04571b079473dfe8ec852b4d74b49163a64
Frank Fang <frankfang2014@hotmail.com>
2019-05-25 10:57:43 -0700

    add dispenseInventory

--- _tests_/vending-machine.spec.js
+++ _tests_/vending-machine.spec.js
@@ -9,21 +9,94 @@ describe("vendingMachine", () => {
     vendingMachine.data = JSON.parse(
       fs.readFileSync("./vendingMachineData.json").toString()
     );
+    vendingMachine.subject = new vendingMachineStats(
+      vendingMachine.data.products,
+      vendingMachine.data.initialCoins
+    );
+    vendingMachine.productsData = vendingMachine.subject.products;
+    vendingMachine.coinsData = vendingMachine.subject.initialCoins;
   });
 
   describe("printInventory()", () => {
-    beforeEach(() => {
-      vendingMachine.subject = new vendingMachineStats(
-        vendingMachine.data.products,
-        vendingMachine.data.initialCoins
-      );
-      vendingMachine.productsData = vendingMachine.subject.products;
-    });
+    //01
     describe("when the user uses vendingMachine", () => {
       it("should return all the vendingMachine items", () => {
         expect(vendingMachine.subject.printInventory()).toEqual(
           vendingMachine.productsData
         );
+      });
+    });
+  });
+  //02
+  describe("refillInventory()", () => {
+    describe("when the inventory is refilled", () => {
+      it("should return all the products with maxiumn quantity", () => {
+        expect(vendingMachine.subject.refillInventory()).toEqual([
+          { id: 1, name: "LV", cost: 2, quantity: 15 },
+          { id: 2, name: "Gucci", cost: 1, quantity: 15 },
+          { id: 3, name: "Prada", cost: 2.25, quantity: 15 },
+          { id: 4, name: "Chanel", cost: 4.75, quantity: 15 }
+        ]);
+      });
+    });
+  });
+  //03
+  describe("resupplyChange()", () => {
+    describe("when the change is resupplied", () => {
+      it("should return all the money with maxiumn quantity", () => {
+        expect(vendingMachine.subject.resupplyChange()).toEqual([
+          { name: "quarter", price: 0.25, quantity: 100 },
+          { name: "dime", price: 0.1, quantity: 100 },
+          { name: "nickel", price: 0.5, quantity: 100 },
+          { name: "loonie", price: 1, quantity: 100 },
+          { name: "toonie", price: 2, quantity: 100 },
+          { name: "fiveDollars", price: 5, quantity: 100 }
+        ]);
+      });
+    });
+  });
+
+  describe("DispenseInventory(payment,item)", () => {
+    //04
+    describe("when user choose the item that is out of stock", () => {
+      it("should throw an error", () => {
+        expect(() =>
+          vendingMachine.subject.DispenseInventory(5, "Chanel")
+        ).toThrow("out of stock");
+      });
+    });
+    //05
+    describe("when user insert the money that is less than the item price", () => {
+      it("should throw an error", () => {
+        expect(() =>
+          vendingMachine.subject.DispenseInventory(1, "Prada")
+        ).toThrow("insufficient payment");
+      });
+    });
+    //06
+    describe("when user insert the money that is greater than 5dollars", () => {
+      it("should throw an error", () => {
+        expect(() =>
+          vendingMachine.subject.DispenseInventory(10, "Gucci")
+        ).toThrow("only accepts the payment that is less equal than 5 dollars");
+      });
+    });
+    //07
+    describe("when user insert the correct payment", () => {
+      it("should return the item with new quantity", () => {
+        expect(vendingMachine.subject.DispenseInventory(2, "Gucci")).toEqual([
+          { id: 1, name: "LV", cost: 2, quantity: 14 },
+          { id: 2, name: "Gucci", cost: 1, quantity: 10 },
+          { id: 3, name: "Prada", cost: 2.25, quantity: 15 },
+          { id: 4, name: "Chanel", cost: 4.75, quantity: 0 }
+        ]);
+      });
+    });
+  });
+  describe("returnChanges(payment,item)", () => {
+    describe("when user finishes the payment", () => {
+      it("should return the total change", () => {
+        expect(vendingMachine.subject.returnChanges(5, "Prada")).toEqual(2.75);
       });
     });
   });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,10 +2,57 @@ class vendingMachineStats {
   constructor(products, initialCoins) {
     this.products = products;
     this.initialCoins = initialCoins;
+    this.maxQuantity = 15;
+    this.maxChangeQuantity = 100;
+    this.maxPayment = 5;
   }
   printInventory() {
     console.log(this.products);
     return this.products;
+  }
+
+  refillInventory() {
+    const refilledInventory = this.products.map(product => {
+      return {
+        ...product,
+        quantity: this.maxQuantity
+      };
+    });
+    return refilledInventory;
+  }
+  resupplyChange() {
+    const resuppliedChange = this.initialCoins.map(coin => {
+      return {
+        ...coin,
+        quantity: this.maxChangeQuantity
+      };
+    });
+    return resuppliedChange;
+  }
+
+  DispenseInventory(payment, item) {
+    const findItem = this.products.find(product => product.name === item);
+    if (findItem.quantity === 0) throw Error("out of stock");
+    if (findItem.cost > payment) throw Error("insufficient payment");
+    if (payment >= this.maxPayment)
+      throw Error("only accepts the payment that is less equal than 5 dollars");
+    let newQuantity = findItem.quantity - 1;
+    const newQuantityProduct = this.products.map(product => {
+      if (product.name === item) {
+        return {
+          ...product,
+          quantity: newQuantity
+        };
+      }
+      return product;
+    });
+    console.log(newQuantityProduct);
+    return newQuantityProduct;
+  }
+  returnChanges(payment, item) {
+    const findItem = this.products.find(product => product.name === item);
+    const change = payment - findItem.cost;
+    return change;
   }
 }
 
