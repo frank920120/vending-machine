8bff1a94ded33ee2acb8a30b2c052339c07fa741
Frank Fang <frankfang2014@hotmail.com>
2019-05-25 14:41:09 -0700

    add refilloneitem function

--- _tests_/vending-machine.spec.js
+++ _tests_/vending-machine.spec.js
@@ -29,7 +29,7 @@ describe("vendingMachine", () => {
   });
   //02
   describe("refillInventory()", () => {
-    describe("when the inventory is refilled", () => {
+    describe("when the inventory needs to be refilled", () => {
       it("should return all the products with maxiumn quantity", () => {
         expect(vendingMachine.subject.refillInventory()).toEqual([
           { id: 1, name: "LV", cost: 2, quantity: 15 },
@@ -41,23 +41,35 @@ describe("vendingMachine", () => {
     });
   });
   //03
+  describe("refillOneItem(item)", () => {
+    describe("when one of the items in the inventory needs to be refilled", () => {
+      it("should return all the products with the item that has been refilled", () => {
+        expect(vendingMachine.subject.refillOneItem("Chanel")).toEqual([
+          { id: 1, name: "LV", cost: 2, quantity: 14 },
+          { id: 2, name: "Gucci", cost: 1, quantity: 11 },
+          { id: 3, name: "Prada", cost: 2.25, quantity: 15 },
+          { id: 4, name: "Chanel", cost: 4.75, quantity: 15 }
+        ]);
+      });
+    });
+  });
+  //04
   describe("resupplyChange()", () => {
     describe("when the change is resupplied", () => {
       it("should return all the money with maxiumn quantity", () => {
         expect(vendingMachine.subject.resupplyChange()).toEqual([
           { name: "quarter", price: 0.25, quantity: 100 },
           { name: "dime", price: 0.1, quantity: 100 },
-          { name: "nickel", price: 0.5, quantity: 100 },
+          { name: "nickel", price: 0.05, quantity: 100 },
           { name: "loonie", price: 1, quantity: 100 },
-          { name: "toonie", price: 2, quantity: 100 },
-          { name: "fiveDollars", price: 5, quantity: 100 }
+          { name: "toonie", price: 2, quantity: 100 }
         ]);
       });
     });
   });
 
   describe("DispenseInventory(payment,item)", () => {
-    //04
+    //05
     describe("when user choose the item that is out of stock", () => {
       it("should throw an error", () => {
         expect(() =>
@@ -65,7 +77,15 @@ describe("vendingMachine", () => {
         ).toThrow("out of stock");
       });
     });
-    //05
+    //06
+    describe("when user choose the item that does not exist", () => {
+      it("should throw an error", () => {
+        expect(() =>
+          vendingMachine.subject.DispenseInventory(5, "hello")
+        ).toThrow("item does not exist");
+      });
+    });
+    //07
     describe("when user insert the money that is less than the item price", () => {
       it("should throw an error", () => {
         expect(() =>
@@ -73,15 +93,8 @@ describe("vendingMachine", () => {
         ).toThrow("insufficient payment");
       });
     });
-    //06
-    describe("when user insert the money that is greater than 5dollars", () => {
-      it("should throw an error", () => {
-        expect(() =>
-          vendingMachine.subject.DispenseInventory(10, "Gucci")
-        ).toThrow("only accepts the payment that is less equal than 5 dollars");
-      });
-    });
-    //07
+
+    //08
     describe("when user insert the correct payment", () => {
       it("should return the item with new quantity", () => {
         expect(vendingMachine.subject.DispenseInventory(2, "Gucci")).toEqual([
@@ -93,10 +106,17 @@ describe("vendingMachine", () => {
       });
     });
   });
+  //09
   describe("returnChanges(payment,item)", () => {
     describe("when user finishes the payment", () => {
       it("should return the total change", () => {
-        expect(vendingMachine.subject.returnChanges(5, "Prada")).toEqual(2.75);
+        expect(vendingMachine.subject.returnChanges(10, "Prada")).toEqual([
+          { name: "quarter", price: 0.25, quantity: 27 },
+          { name: "dime", price: 0.1, quantity: 35 },
+          { name: "nickel", price: 0.05, quantity: 40 },
+          { name: "loonie", price: 1, quantity: 44 },
+          { name: "toonie", price: 2, quantity: 47 }
+        ]);
       });
     });
   });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -10,7 +10,17 @@ class vendingMachineStats {
     console.log(this.products);
     return this.products;
   }
+  refillOneItem(item) {
+    const currentItem = this.products.map(product => {
+      if (product.name === item) {
+        product.quantity = this.maxQuantity;
+        return product;
+      }
+      return product;
+    });
 
+    return currentItem;
+  }
   refillInventory() {
     const refilledInventory = this.products.map(product => {
       return {
@@ -32,10 +42,10 @@ class vendingMachineStats {
 
   DispenseInventory(payment, item) {
     const findItem = this.products.find(product => product.name === item);
+    if (!findItem) throw Error("item does not exist");
     if (findItem.quantity === 0) throw Error("out of stock");
     if (findItem.cost > payment) throw Error("insufficient payment");
-    if (payment >= this.maxPayment)
-      throw Error("only accepts the payment that is less equal than 5 dollars");
+
     let newQuantity = findItem.quantity - 1;
     const newQuantityProduct = this.products.map(product => {
       if (product.name === item) {
@@ -46,13 +56,89 @@ class vendingMachineStats {
       }
       return product;
     });
-    console.log(newQuantityProduct);
+
     return newQuantityProduct;
   }
   returnChanges(payment, item) {
+    let quarterCount = 0;
+    let dimeCount = 0;
+    let nickelCount = 0;
+    let loonieCount = 0;
+    let toonieCount = 0;
     const findItem = this.products.find(product => product.name === item);
     const change = payment - findItem.cost;
-    return change;
+    const int = Math.floor(change);
+    const float = change % int;
+    if (int % 2 == 0) {
+      toonieCount = int / 2;
+    }
+    if (int % 2 != 0) {
+      loonieCount = 1;
+      toonieCount = (int - 1) / 2;
+    }
+    if (float > 0.25) {
+      quarterCount = Math.floor(float / 0.25);
+      let quaterReminder = float % 0.25;
+      if (quaterReminder >= 0.1) {
+        dimeCount = Math.floor(float / 0.1);
+        let dimeReminder = float % 0.1;
+        if (dimeReminder >= 0.05) {
+          nickelCount = Math.floor(float / 0.05);
+        }
+      }
+    }
+    if (float < 0.25) {
+      dimeCount = Math.floor(float / 0.1);
+      let dimeReminder = float % 0.1;
+      if (dimeReminder >= 0.05) {
+        nickelCount = Math.floor(float / 0.05);
+      }
+    }
+    if (float < 0.1) {
+      nickelCount = Math.floor(float / 0.05);
+    }
+
+    const coins = this.initialCoins.map(coin => {
+      if (coin.name === "quarter") {
+        return {
+          ...coin,
+          quantity: coin.quantity - quarterCount
+        };
+      }
+      if (coin.name === "dime") {
+        return {
+          ...coin,
+          quantity: coin.quantity - dimeCount
+        };
+      }
+      if (coin.name === "nickel") {
+        return {
+          ...coin,
+          quantity: coin.quantity - nickelCount
+        };
+      }
+      if (coin.name === "loonie") {
+        return {
+          ...coin,
+          quantity: coin.quantity - loonieCount
+        };
+      }
+      if (coin.name === "toonie") {
+        return {
+          ...coin,
+          quantity: coin.quantity - toonieCount
+        };
+      }
+    });
+    console.log({
+      nickelCount,
+      dimeCount,
+      quarterCount,
+      loonieCount,
+      toonieCount,
+      totalChange: change
+    });
+    return coins;
   }
 }
 
